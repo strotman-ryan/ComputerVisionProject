576c3c0d097cca68549aafe9328eec0e78121632
strotman-ryan <strotman.7@osu.edu>
2020-11-23 15:53:01 -0500

    view video

--- AnalyzeVideo.py
+++ AnalyzeVideo.py
@@ -22,6 +22,7 @@ video_object = cv2.VideoCapture(video_path)
 frames = []
 ret,frame = video_object.read() 
 while(ret) :
+    cv.imshow("video", frame)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frames.append(gray)
     ret,frame = video_object.read()  
