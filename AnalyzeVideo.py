d4488981d6d19178af9de70155000d8b76254fb4
strotman-ryan <strotman.7@osu.edu>
2020-11-23 20:42:45 -0500

    added data processing

--- AnalyzeVideo.py
+++ AnalyzeVideo.py
@@ -2,16 +2,37 @@
 from time import sleep
 import cv2 
 import numpy as np
+from skimage.color import rgb2yiq
+from scipy.ndimage import binary_closing
 
 def Standardize(image):
     xMin = image.min()
     xMax = image.max()
     return (image - xMin) /(xMax - xMin)
 
+#return true if ball in image
+#return false if no ball
+def RemoveSmallComponents(image):
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(difference.astype(np.uint8), connectivity=8)
+    if nb_components < 2:
+        return False, None
+    index = np.argsort(stats[:,4])[-2] #take the second largest, the first is the background
+    if stats[:,4][index] < 30:
+        #area needs to be greater than 30 pixels
+        return False, None
+    img2 = np.zeros((output.shape))
+    for i in range(0, nb_components):
+        if i == index:
+            img2[output == i] = 1
+    return True, img2
+
+#YIQ image
+#range of I and Q coordinates are -.59 to .59
 def ImagedDifference(img1, img2, threshold):
     difference = img1 - img2
-    difference = np.absolute(difference)
-    difference = Standardize(difference)
+    difference = np.square(difference)
+    difference = difference[:,:,1] + difference[:,:,2]
+    difference = np.sqrt(difference)
     difference[difference > threshold] = 1
     difference[difference <= threshold] = 0
     return difference
@@ -19,20 +40,46 @@ def ImagedDifference(img1, img2, threshold):
 video_path = "./Video/video.h264"
 video_object = cv2.VideoCapture(video_path)
 
-frames = []
+yiqFrames = []
+rgbFrames = []
 ret,frame = video_object.read() 
+numFrames = 0
 while(ret) :
-    cv2.imshow("video", frame)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frames.append(gray)
+    numFrames += 1
+    rgbFrames.append(frame)
+    yiqFrames.append(rgb2yiq(frame))
+    # Press Q on keyboard to  exit
+    #this makes the video play for some reason
+    '''
+    if cv2.waitKey(25) & 0xFF == ord('q'): 
+      break
+    cv2.imshow("Frame", frame)
+    '''
+    #140 250
+    #if numFrames % 10 ==0 and numFrames >= 140 and numFrames <= 250:
+    if numFrames == 160:
+        cv2.imshow("FrameRGB: " + str(numFrames), rgbFrames[-1])
+        difference = ImagedDifference(yiqFrames[0], yiqFrames[-1], .05) 
+        cv2.imshow("FrameDifference: " + str(numFrames),difference)
+        valid, difference = RemoveSmallComponents(difference)
+        if valid:
+            cv2.imshow("One Connected Component: " + str(numFrames), difference)           
+            difference = binary_closing(difference, iterations=10).astype(float)
+            cv2.imshow("After closing: " + str(numFrames), difference)
+            nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(difference.astype(np.uint8), connectivity=8)
+            print(stats)
+        else:
+            print("Frame "  +str(numFrames)  + " not valid")
+        break
+
     ret,frame = video_object.read()  
 
-firstFrame = frames[0]
-lastFrame = frames[len(frames) -1]
-difference = ImagedDifference(firstFrame, lastFrame, .7)
-print(difference)
-cv2.imshow("difference", difference)
-cv2.imshow('1', firstFrame)
-cv2.imshow('last', lastFrame)
 
+#.05 is best for threshold
+'''
+for threshold in range(4,10):
+    threshold = threshold / 100
+    difference = ImagedDifference(yiqFrames[0], yiqFrames[-1], threshold)
+    cv2.imshow("difference"  + str(threshold), difference)
+'''
 cv2.waitKey(0)
